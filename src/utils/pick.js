fb47490cbc4d9c82aaedd23b44e5c02029ecc8ba
Aashish Bhagwat <aashishbhagwat@Aashishs-MBP.domain.name>
2024-03-14 11:09:49 +0530

    updated pick JS

--- src/utils/pick.js
+++ src/utils/pick.js
@@ -7,8 +7,13 @@
 const pick = (object, keys) => {
   return keys.reduce((obj, key) => {
     if (object && Object.prototype.hasOwnProperty.call(object, key)) {
-      // eslint-disable-next-line no-param-reassign
-      obj[key] = object[key];
+      if (typeof object[key] === 'string') {
+        // Convert the value to a regex pattern for case-insensitive substring matching
+        // We updated this stuff
+        obj[key] = { $regex: new RegExp(object[key], 'i') };
+      } else {
+        obj[key] = object[key];
+      }
     }
     return obj;
   }, {});
